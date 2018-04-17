a3f34f59037064949dbc6c18505871f1990fc1dc
Weekend Master <weekend@super.com>
2018-04-17 16:44:25 +0300

    fixed MovieAdapter and Intent

--- app/src/main/java/com/exampleapp/movieselector/MainActivity.java
+++ app/src/main/java/com/exampleapp/movieselector/MainActivity.java
@@ -1,11 +1,12 @@
 package com.exampleapp.movieselector;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
 
 import java.util.ArrayList;
-import java.util.Vector;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,9 +19,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ArrayList<Movie> movies = new ArrayList<>();
-        movies.add(new Movie("Back to the Future", "url 1", 1985));
-        movies.add(new Movie("Back to the Future II", "url 2", 1986));
-        movies.add(new Movie("Back to the Future III", "url 3", 1991));
+        movies.add(new Movie("Back to the Future", "url 1", 2001));
+        movies.add(new Movie("Back to the Future II", "url 2", 2002));
+        movies.add(new Movie("Back to the Future III", "url 3", 2003));
 
 
         movieAdapter = new MovieAdapter(this, R.layout.listitem_movie, movies);
@@ -28,5 +29,10 @@ public class MainActivity extends AppCompatActivity {
         movieList = (ListView) findViewById(R.id.moviesList);
         movieList.setAdapter(movieAdapter);
 
+    }
+
+    public void onFavorites(View view) {
+        Intent intent = new Intent(MainActivity.this, Favorites.class);
+        startActivity(intent);
     }
 }
