a9cae752a0ac3dd1ddbb5b3fa4c23d1496be7106
Weekend Master <weekend@super.com>
2018-04-18 07:33:57 +0300

    http unit test

--- app/src/main/java/com/exampleapp/movieselector/MainActivity.java
+++ app/src/main/java/com/exampleapp/movieselector/MainActivity.java
@@ -3,6 +3,7 @@ package com.exampleapp.movieselector;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -33,7 +34,8 @@ public class MainActivity extends AppCompatActivity {
         HttpAsync httpAsync = new HttpAsync();
         httpAsync.execute("https://www.omdbapi.com/?apikey=69d51abd&t=back");
 
-        Toast.makeText(this, httpAsync.getResponse(), Toast.LENGTH_SHORT).show();
+//        Log.i("[HTTP RESPONSE]", httpAsync.getResponse());
+//        Toast.makeText(this, httpAsync.getResponse(), Toast.LENGTH_SHORT).show();
     }
 
     public void onFavorites(View view) {
--- app/src/test/java/com/exampleapp/movieselector/test_HttpAsync.java
+++ app/src/test/java/com/exampleapp/movieselector/test_HttpAsync.java
@@ -1,10 +1,11 @@
 package com.exampleapp.movieselector;
 
+import android.util.Log;
 
-import android.os.AsyncTask;
+import org.junit.Test;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-import java.io.IOError;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -12,41 +13,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-
-public class HttpAsync extends AsyncTask<String, Integer, String> {
-
-    String response;
-
-    @Override
-    protected String doInBackground(String ... urls) {
-
-        URL url;
-        HttpURLConnection connection = null;
-
-        try {
-            url = new URL(urls[0]);
-            connection = (HttpURLConnection) url.openConnection();
-            int responseCode = connection.getResponseCode();
-            if(responseCode == HttpURLConnection.HTTP_OK) {
-                response = readStream(connection.getInputStream());
-            }
-        }
-        catch(MalformedURLException e) {
-            e.printStackTrace();
-        }
-        catch(IOException e) {
-            e.printStackTrace();
-        }
-
-        return null;
-    }
-
-
-    @Override
-    protected void onPostExecute(String result) {
-        super.onPostExecute(result);
-    }
-
+public class test_HttpAsync {
 
     private String readStream(InputStream input) {
 
@@ -78,9 +45,20 @@ public class HttpAsync extends AsyncTask<String, Integer, String> {
         return responseBuffer.toString();
     }
 
+    @Test
+    public void connectionIsOk() throws MalformedURLException, IOException {
 
-    public String getResponse() {
-        return response;
+        URL url;
+        HttpURLConnection urlConnection;
+
+        url = new URL("https://www.omdbapi.com/?apikey=69d51abd&t=back");
+        urlConnection = (HttpURLConnection) url.openConnection();
+        InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+        String response = readStream(in);
+        urlConnection.disconnect();
+
+        System.out.println(response);
+
     }
 
 }
